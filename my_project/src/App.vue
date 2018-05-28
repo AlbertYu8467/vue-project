a3fac76889518c1ac4b8227e32e7229b49336e3c
yuhu1991 <846788588@qq.com>
2018-05-28 22:33:07 +0800

    first commit

---
+++ my_project/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <el-menu :default-active='activeIndex' class="el-menu-demo" mode='horizontal' :router=true>
+          <el-menu-item index='/'>首页</el-menu-item>
+          <el-submenu index='moive'>
+            <template slot='title'>娱乐</template>
+            <el-menu-item index='moive'>电影</el-menu-item>
+          </el-submenu>
+          <el-submenu index='history'>
+            <template slot="title">知识</template>
+            <el-menu-item index='history'>历史</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-header>
+      <el-main>
+         <router-view/>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return {
+        activeIndex:'/'
+    }
+  },
+  methods:{
+    handleSelect(key,paht){
+      console.log(key,path)
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  background: #f6f6f6;  
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  width: 64%;
+  margin: 0 auto;
+  background: #fff;
+
+}
+</style>
---
+++ my_project/src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Index from '@/components/Index'
+import Moive from '@/components/Moive'
+import History from '@/components/History'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Index',
+      component: Index
+    },
+    {
+      path:'/moive',
+      name:'Moive',
+      component:Moive
+    },
+    {
+      path:'/history',
+      name:'History',
+      component:History
+    }
+  ]
+})
